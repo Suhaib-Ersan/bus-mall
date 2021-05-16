9bbff6d8eb52f93ef534a73f724c877b9001e343
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-05-16 22:03:39 +0300

    finished

--- js/app.js
+++ js/app.js
@@ -1,60 +1,100 @@
-'use strict';
+"use strict";
 
 let attempts = 0;
 let maxAttempts = 25;
-let attemptsHtmlEl = document.getElementById('attemptNumber');
+let attemptsHtmlEl = document.getElementById("attemptNumber");
 
-let assets = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'logo_image.png', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg']
+let assets = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "water-can.jpg"];
 
 let choicesArray = [];
 
 function ChoicesArrayConstructor(picName) {
-  this.picName = picName.split('.')[0];
-  this.assetPath = 'assets/' + picName;
+  this.picName = picName.split(".")[0];
+  this.assetPath = "assets/" + picName;
   this.clicks = 0;
   this.views = 0;
   choicesArray.push(this);
 }
 
 for (let i = 0; i < assets.length; i++) {
-    new ChoicesArrayConstructor(assets[i]);
-    
+  new ChoicesArrayConstructor(assets[i]);
 }
 
 function generateRandomNumber() {
-    return Math.floor(Math.random() * choicesArray.length);
+  return Math.floor(Math.random() * choicesArray.length);
 }
 
-let img1 = document.getElementById('img1');
-let img2 = document.getElementById('img2');
-let img3 = document.getElementById('img3');
+let img1El = document.getElementById("img1");
+let img2El = document.getElementById("img2");
+let img3El = document.getElementById("img3");
 
-let currentImg1;
-let currentImg2;
-let currentImg3;
+let currentimg1;
+let currentimg2;
+let currentimg3;
+
+// console.log(img1El);
+// console.log(img2El);
+// console.log(img3El);
+
+// console.log(currentimg1);
+// console.log(currentimg2);
+// console.log(currentimg3);
 
 function renderImages() {
-    currentImg1 = generateRandomNumber();
-    currentImg2 = generateRandomNumber();
-    currentImg3 = generateRandomNumber();
+  currentimg1 = generateRandomNumber();
+  currentimg2 = generateRandomNumber();
+  currentimg3 = generateRandomNumber();
 
-    while (currentImg1 === currentImg2 || currentImg1 === currentImg3 || currentImg2 === currentImg3) {
-        currentImg1 = generateRandomNumber();
-        currentImg2 = generateRandomNumber();
-        currentImg3 = generateRandomNumber();
+  while (currentimg1 === currentimg2 || currentimg1 === currentimg3 || currentimg2 === currentimg3) {
+    currentimg1 = generateRandomNumber();
+    currentimg2 = generateRandomNumber();
+    currentimg3 = generateRandomNumber();
+  }
+
+  img1El.setAttribute("src", choicesArray[currentimg1].assetPath);
+  img1El.setAttribute("title", choicesArray[currentimg1].assetPath);
+  choicesArray[currentimg1].views++;
+
+  img2El.setAttribute("src", choicesArray[currentimg2].assetPath);
+  img2El.setAttribute("title", choicesArray[currentimg2].assetPath);
+  choicesArray[currentimg2].views++;
+
+  img3El.setAttribute("src", choicesArray[currentimg3].assetPath);
+  img3El.setAttribute("title", choicesArray[currentimg3].assetPath);
+  choicesArray[currentimg3].views++;
+
+  attemptsHtmlEl.textContent = attempts;
+}
+
+renderImages();
+
+img1El.addEventListener("click", imageClickFunction);
+img2El.addEventListener("click", imageClickFunction);
+img3El.addEventListener("click", imageClickFunction);
+
+function imageClickFunction(event) {
+  attempts++;
+  if (attempts <= maxAttempts) {
+
+    if (event.target.id === 'img1') {
+        choicesArray[currentimg1].clicks++; 
+    }else if (event.target.id === 'img2') {
+        choicesArray[currentimg2].clicks++;
+    }else if (event.target.id === 'img3') {
+        choicesArray[currentimg3].clicks++;
     }
+    renderImages();
+  } else {
+      let resultsUlEl = document.getElementById('resultsUl');
+      let resultsLlEl;
+      for (let i = 0; i < choicesArray.length; i++) {
+          resultsLlEl = document.createElement('li');
+          resultsUlEl.appendChild(resultsLlEl);
+          resultsLlEl.textContent = `${choicesArray[i].picName} had ${choicesArray[i].clicks} votes, and was seen ${choicesArray[i].views} times.`
+      }
 
-    img1.setAttribute('src', ChoicesArrayConstructor[currentImg1].assetPath);
-    img1.setAttribute('title', ChoicesArrayConstructor[currentImg1].assetPath);
-    ChoicesArrayConstructor[currentImg1].views++;
-
-    img2.setAttribute('src', ChoicesArrayConstructor[currentImg2].assetPath);
-    img2.setAttribute('title', ChoicesArrayConstructor[currentImg2].assetPath);
-    ChoicesArrayConstructor[currentImg2].views++;
-
-    img3.setAttribute('src', ChoicesArrayConstructor[currentImg3].assetPath);
-    img3.setAttribute('title', ChoicesArrayConstructor[currentImg3].assetPath);
-    ChoicesArrayConstructor[currentImg3].views++;
-
-
+      img1El.removeEventListener("click", imageClickFunction);
+      img2El.removeEventListener("click", imageClickFunction);
+      img3El.removeEventListener("click", imageClickFunction);
+  }
 }
