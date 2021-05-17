1775bb926c7eec939431614c3b37953babee69d3
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-05-17 17:37:32 +0300

    made images stop repeating, probably finished

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let attempts = 0;
-let maxAttempts = 5;
+let maxAttempts = 25;
 let attemptsHtmlEl = document.getElementById("attemptNumber");
 
 let assets = ["bag.jpg", "banana.jpg", "bathroom.jpg", "boots.jpg", "breakfast.jpg", "bubblegum.jpg", "chair.jpg", "cthulhu.jpg", "dog-duck.jpg", "dragon.jpg", "pen.jpg", "pet-sweep.jpg", "scissors.jpg", "shark.jpg", "sweep.png", "tauntaun.jpg", "unicorn.jpg", "water-can.jpg"];
@@ -21,8 +21,6 @@ function ChoicesArrayConstructor(picName) {
   this.views = 0;
 
   choicesArray.push(this);
-  clicksDataArray.push(this.clicks);
-  viewsDataArray.push(this.views);
   picNamesArray.push(this.picName);
 }
 
@@ -42,6 +40,10 @@ let currentimg1;
 let currentimg2;
 let currentimg3;
 
+let futureimg1;
+let futureimg2;
+let futureimg3;
+
 // console.log(img1El);
 // console.log(img2El);
 // console.log(img3El);
@@ -55,11 +57,16 @@ function renderImages() {
   currentimg2 = generateRandomNumber();
   currentimg3 = generateRandomNumber();
 
-  while (currentimg1 === currentimg2 || currentimg1 === currentimg3 || currentimg2 === currentimg3) {
+  while (currentimg1 === futureimg1 || currentimg1 === futureimg2 || currentimg1 === futureimg3 || currentimg2 === futureimg1 || currentimg2 === futureimg2 || currentimg2 === futureimg3 || currentimg3 === futureimg1 || currentimg3 === futureimg2 || currentimg3 === futureimg3 || currentimg1 === currentimg2 || currentimg1 === currentimg3 || currentimg2 === currentimg3) {
     currentimg1 = generateRandomNumber();
     currentimg2 = generateRandomNumber();
     currentimg3 = generateRandomNumber();
   }
+
+  futureimg1 = currentimg1;
+  futureimg2 = currentimg2;
+  futureimg3 = currentimg3;
+
 
   img1El.setAttribute("src", choicesArray[currentimg1].assetPath);
   img1El.setAttribute("title", choicesArray[currentimg1].assetPath);
@@ -86,7 +93,8 @@ let showResultsButtonEl = document.getElementById("showResultsButton");
 
 function imageClickFunction(event) {
   attempts++;
-  if (attempts <= maxAttempts) {
+  attemptsHtmlEl.textContent = attempts;
+  if (attempts < maxAttempts) {
     if (event.target.id === "img1") {
       choicesArray[currentimg1].clicks++;
     } else if (event.target.id === "img2") {
@@ -96,17 +104,34 @@ function imageClickFunction(event) {
     }
     renderImages();
   } else {
+    if (event.target.id === "img1") {
+      choicesArray[currentimg1].clicks++;
+    } else if (event.target.id === "img2") {
+      choicesArray[currentimg2].clicks++;
+    } else if (event.target.id === "img3") {
+      choicesArray[currentimg3].clicks++;
+    }
     showResultsButtonEl.removeAttribute("disabled");
     showResultsButtonEl.addEventListener("click", showResultsPanel);
 
     img1El.removeEventListener("click", imageClickFunction);
     img2El.removeEventListener("click", imageClickFunction);
     img3El.removeEventListener("click", imageClickFunction);
+
+    for (let i = 0; i < choicesArray.length; i++) {
+
+      clicksDataArray.push(choicesArray[i].clicks);
+      viewsDataArray.push(choicesArray[i].views);
+      
+      
+    }
+
   }
 }
 
 function showResultsPanel() {
-  let canvasHtmlVar = '<div id="sideResultsPanel"><h3>Result</h3><div id="ulResultsCard"><ul id="resultsUl"></ul></div></div><canvas id="myChart" width="auto" height="400px"></canvas><canvas id="myChartTwo" width="auto" height="400px"></canvas>';
+  let canvasHtmlVar = '<div id="sideResultsPanel"><h3>Result</h3><div id="ulResultsCard"><ul id="resultsUl"></ul></div></div><div id="myChartContainer"><canvas id="myChart"></canvas></div>';
+  // <canvas id="myChartTwo"></canvas>
 
   let canvasesContainerEl = document.getElementById("canvasesContainer");
   canvasesContainerEl.innerHTML = canvasHtmlVar;
@@ -121,54 +146,58 @@ function showResultsPanel() {
 
   var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
-    type: "doughnut",
+    type: "bar",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: picNamesArray,
       datasets: [
         {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+          label: "number of clicks",
+          data: clicksDataArray,
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+        {
+          label: "number of views",
+          data: viewsDataArray,
+          backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+          borderColor: ["rgba(54, 162, 235, 1)"],
           borderWidth: 1,
         },
       ],
     },
     options: {
+
+      maintainAspectRatio: false,
+
       scales: {
-        y: {
-          beginAtZero: true,
+        x: {
+          
         },
-      },
-      responsive: false,
+        y: {
+          
+          ticks: {
+            // forces step size to be 50 units
+            stepSize: 1
+          }, 
+          //   gridLines: {
+          //   color: "transparent",
+          //   display: true,
+          //   drawBorder: false,
+          //   zeroLineColor: "#ccc",
+          //   zeroLineWidth: 1
+          // }
+        }
+        
+      }
+      // scales: {
+
+      //   y: {
+
+      //     beginAtZero: true,
+      //   },
+      // },
+      // responsive: false,
     },
   });
-
-  var ctx = document.getElementById("myChartTwo").getContext("2d");
-  var myChart = new Chart(ctx, {
-    type: "doughnut",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-      responsive: false,
-    },
-  });
-
-  showResultsButtonEl.removeEventListener("click", showResultsPanel);
-
 }
